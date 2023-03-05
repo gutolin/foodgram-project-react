4591c252511cecf5802667fc271bcd001b6d7b69
gutolin <gutolin12@yandex.ru>
2023-03-05 23:20:30 +0300

    Исправление подписок

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -129,18 +129,11 @@ class FollowSerializers(serializers.ModelSerializer):
 
     def get_recipes(self, obj):
         request = self.context.get('request')
-        recipes_limit = request.query_params.get('recipes_limit')
-        if not recipes_limit:
-            return RecipeSubscriberSerializers(
-                Recipe.objects.filter(author=obj.author),
-                many=True,
-                context={'request': request}
-            ).data
-        return RecipeSubscriberSerializers(
-            Recipe.objects.filter(author=obj)[:int(recipes_limit)],
-            many=True,
-            context={'request': request}
-        ).data
+        limit = request.GET.get('recipes_limit')
+        queryset = Recipe.objects.filter(author=obj.name)
+        if limit:
+            queryset = queryset[:int(limit)]
+        return RecipeSubscriberSerializers(queryset, many=True).data
 
     def get_recipes_count(self, obj):
         return Recipe.objects.filter(author=obj.author).count()
